d69b14688c2cec33c983b8af402dec9728f023d0
Sav <98275089+Lunaliar@users.noreply.github.com>
2022-11-07 10:48:09 -0500

    update about img 3

--- components/About.jsx
+++ components/About.jsx
@@ -12,7 +12,7 @@ const tw = {
 	aboutPLink: "py-2 text-gray-600 underline cursor-pointer text-[#5651e5]",
 	imageContainer:
 		"w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300",
-	image: "rounded-xl",
+	image: "rounded-xl ",
 };
 
 function About() {
